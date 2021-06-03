91d3de3d79aa08f3b8faf8ef92b2385dec5cdb5e
andrescondo <1dkevinandres.b@gmail.com>
2021-06-02 21:58:30 -0500

    añadido estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
+import '../styles/components/Footer.css';
+
 const Footer = () =>{
   return(
     <div className="Footer">
       <div className="Footer-info">
-        <h2>
-          Footer
-        </h2>
+        <h2> Datos de contacto</h2>
+        <div className="footer-info__contact">
+          <p>Fandub</p>
+          <p>Ecuador</p>
+          <p>Correo</p>
+        </div>
       </div>
       <div className="Footer-social">
-        <h2>
-          Redes Sociales
-        </h2>
+        <h2>Redes Sociales</h2>
+        <div className="social-media">
+          <p className="social-media__link"></p>
+          <p className="social-media__link"></p>
+          <p className="social-media__link"></p>
+          <p className="social-media__link"></p>
+        </div>
+        
       </div>
     </div>
   )
