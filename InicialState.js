775cd4915e9fe21ef08c7bc0812985bbea758621
andrescondo <1dkevinandres.b@gmail.com>
2021-06-03 17:04:13 -0500

    init the inicialstate, and create structure the data

---
+++ InicialState.js
@@ -0,0 +1,51 @@
+
+const InicialState = {
+  dino:[
+    {
+      voiceover:[
+        {
+          id:1,
+          name: '',
+          description: '',
+          url_video:''
+        },
+        {
+          id:2,
+          name: '',
+          description: '',
+          url_video:''
+        },
+        {
+          id:3,
+          name: '',
+          description: '',
+          url_video:''
+        },
+        {
+          id:4,
+          name: '',
+          description: '',
+          url_video:''
+        },
+      ],
+      card:[
+        {
+          id:1,
+          url_img:'',
+          name:'',
+        },
+        {
+          id:2,
+          url_img:'',
+          name:'',
+        },
+        {
+          id:3,
+          url_img:'',
+          name:'',
+        },
+      ]
+    }
+  ],
+
+}
